e862f3968eb5fad2e5a2ba9995a55a3afe64443e
Andrew Nichols <tandrewnichols@gmail.com>
2013-07-13 23:40:53 -0400

    Finish object-ifying

--- forge.js
+++ forge.js
@@ -2,13 +2,10 @@ var neo = require('neo4j')
   , async = require('async')
   , fs = require('fs')
   , EventEmitter = require('events').EventEmitter
-  , extend = require('xtend')
-  , filemap = {}
-  , db;
+  , extend = require('xtend');
   
 var defaults = {
 	dbString: "http://localhost:7474",
-	protoDir: __dirname + "/models",
 	recursive: true,
 	namespace: false,
 	filter: []
@@ -20,59 +17,44 @@ var defaults = {
 var Forge = module.exports = function Forge(options) {
 	var self = this;
 	var opts = extend(defaults, options);
-	db = new neo.GraphDatabase(opts.dbString);
+	self.db = new neo.GraphDatabase(opts.dbString);
 	
 	// define the node object as a getter so that 'this' works correctly
 	self.__defineGetter__('node', function(){
-		return {
-			/*
-			 * Pass in an object with any properties to save a node in neo
-			 * and make that node into an object with a prototype of methods
-			 */
-			fromObj: function(req, obj, callback) {
-				var reqs = req.required || req.required_fields || req.reqs || [];
-				self.validate(obj, reqs, function(err){
+		return {		
+			fromObj: function(name, obj, callback) {
+				self.validate(obj, self.map[name].schema.nodes[name].required, function(err){
 					var node = db.createNode(obj);
 					node.save(function(err, n){
-						self.spawn(n, req, callback);
+						async.each(self.map[name].schema.nodes[name].indexed, function(item, f){
+							node.index(name, item, node._data.data[item], f)
+						}, function(err){
+							self.spawn(name, n, callback);
+						})
 					});
 				});
 			},
 			
-			/*
-			 * Pass in an id to get an existing node from neo and construct a
-			 * prototypal object with it
-			 */
-			fromId: function(req, id, callback) {
+			fromId: function(name, id, callback) {
 				db.getNodeById(id, function(err, n){
-					self.spawn(n, req, callback);
+					self.spawn(name, n, callback);
 				});
 			},
 			
-			/*
-			 * Get a prototypal object from a neo index
-			 */
-			fromIndex: function(req, index, key, value, callback) {
+			fromIndex: function(name, index, key, value, callback) {
 				db.getIndexedNodes(index, key, value, function(err, results){
-					self.spawn(results[0], req, callback);
+					self.spawn(name, results[0], callback);
 				});
 			},
 			
-			/*
-			 * Methods pertaining to sets of objects
-			 */
 			set: {
-				
-				/*
-				 * Corresponds to fromObj, but for sets of objects
-				 */
-				fromObjs: function(req, objs, options, callback) {
+				fromObjs: function(name, objs, options, callback) {
 					if (typeof (options) === 'function') callback = options, options = {};
 					var parallel = typeof(options.parallel) === 'undefined' ? true : options.parallel;
 					var method = parallel ? 'each' : 'eachSeries';
 					var results = [];
 					async[method](objs, function(item, f){
-						self.node.fromObj(req, item, function(err, obj) {
+						self.node.fromObj(name, item, function(err, obj) {
 							results.push(obj);
 							f();
 						});
@@ -81,14 +63,22 @@ var Forge = module.exports = function Forge(options) {
 					});
 				},
 				
-				/*
-				 * Corresponds to fromId, but for sets of objects
-				 */
-				fromIds: function(req, ids, options, callback) {
+				fromIds: function(name, ids, options, callback) {
 					if (typeof (options) === 'function') callback = options, options = {};
 					var parallel = typeof(options.parallel) === 'undefined' ? true : options.parallel;
 					var method = parallel ? 'each' : 'eachSeries';
 					var results = [];
+					var process = function(err) {
+						async[method](ids, function(i, f){
+							self.node.fromId(name, i, function(err, obj){
+								results.push(obj);
+								f();
+							});
+						}, function(err){
+							self.parseSet(options, results, callback);
+						});
+					}
+					
 					if (typeof (ids) === 'string') {
 						var ends = ids.split('-');
 						ids = [];
@@ -98,33 +88,21 @@ var Forge = module.exports = function Forge(options) {
 								ids.push(ends[0]++);
 								callback();
 							},
-							function(err){
-								async[method](ids, function(i, f){
-									self.node.fromId(req, i, function(err, obj){
-										results.push(obj);
-										f();
-									});
-								}, function(err){
-									self.parseSet(options, results, callback);
-								});
-							}
+							process
 						);
+					} else {
+						process(null);
 					}
 				},
 				
-				/*
-				 * Still takes only one index, key, and value, but returns all matching
-				 * results, rather than just the first. Prefer this if the results of
-				 * an indexed search are not guaranteed to be unique.
-				 */
-				fromIndex: function(req, index, key, value, options, callback) {
+				fromIndex: function(name, index, key, value, options, callback) {
 					if (typeof options === 'function') callback = options, options = {};
 					var parallel = typeof(options.parallel) === 'undefined' ? true : options.parallel;
 					var method = parallel ? 'each' : 'eachSeries';
 					var results = [];
 					db.getIndexedNodes(index, key, value, function(err, results){
 						async[method](results, function(item, f){
-							self.spawn(item, req, function(err, obj){
+							self.spawn(name, item, function(err, obj){
 								results.push(obj);
 								f();
 							});
@@ -134,29 +112,21 @@ var Forge = module.exports = function Forge(options) {
 					});
 				},
 				
-				/*
-				 * Create multiple objects with the same values. Useful if objects tend
-				 * to have several values in common. You can then add unique properties later.
-				 */
-				fromTemplate: function(req, obj, count, callback) {
+				fromTemplate: function(name, obj, count, callback) {
 					var reqs = req.required || req.required_fields || req.reqs || [];
-					self.validate(obj, reqs, function(err){
+					self.validate(obj, self.map[name].schema.nodes[name].required, function(err){
 						if (err) callback(err);
 						async.times(count, function(i, next){
-							self.node.fromObj(req, obj, next);
+							self.node.fromObj(name, obj, next);
 						}, callback);
 					});
 				},
 				
-				/*
-				 * Gets a set of nodes that matches any passed in query.
-				 * You can pass in either cypher or a lucene index query.
-				 */
 				fromQuery: function(req, cypher, vars, callback) {
 					var results = [];
 					var handle_results = function(err, nodes){
 						async.each(nodes, function(item, f){
-							self.spawn(item, req, function(err, obj){
+							self.spawn(name, item, function(err, obj){
 								results.push(item);
 								f();
 							}, function(err){
@@ -173,44 +143,38 @@ var Forge = module.exports = function Forge(options) {
 	});
 	
 	// recursive method to load a directory
-	var read = function(root, cb) {
-		
-		// get all files in currect directory
-		fs.readdir(root, function(err, files){
-			
-			// and for each, in tandem
-			async.each(files, function(file, f){
-				
-				// get info
-				fs.stat(file, function(err, stat){
-					
-					// recurse if it's another directory
-					if (opts.recursive && stat.isDirectory()) {
-						read(root + '/' + file, cb);
-					} else {
-						
-						if (opts.filter.indexOf(file) !== -1) {
-							// or require the module, add it to filemap so we can
-							// reference it later, and build the custom methods.
-							var req = require(root + '/' + file);
-							
-							// TODO: Add namespacing here
-							filemap[file] = req;
-							self.buildMethods(req, file, function(methods){
-								self[file] = methods;
-								f();
-							});
-						} else f();
+	var read = function(root, cb) {		
+		async.each(Object.keys(self.schema.nodes), function(file, f){
+			fs.exists(root + '/' + file, function(err, exists){
+				if (exists) {
+					self.map[file] = {
+						proto: require(root + '/' + file),
+						schema: self.schema.nodes[file]
 					}
-				});
-			}, cb);
+					self.buildMethods(req, file, function(methods){
+						self[file] = methods;
+						f();
+					});
+				} else {
+					// TODO: Add namespace checking?
+					f();
+				}
+			});
 		});
 	};
 	
-	// initial call to either the passed in directory or the default
-	read(opts.protoDir, function(err){
-		self.emit('loaded');
-	});
+	if(opts.schemaDir) {
+		self.schema = require(opts.schemaDir + '/schema.json');
+		read(opts.schemaDir, function(err){
+			self.emit('loaded');
+		});
+	} else {
+		self.getSchemaDir(function(err, dir){
+			read(dir, function(err){
+				self.emit('loaded');
+			});
+		});
+	}
 };
 
 /*
@@ -222,11 +186,11 @@ Forge.prototype.__proto__ = EventEmitter.prototype;
  * Create an object from a prototype and add a variety of properties
  * and additional functions
  */
-Forge.prototype.spawn = function(node, req, cb) {
+Forge.prototype.spawn = function(name, node, cb) {
 	var self = this;
-	
+	var proto = typeof name === object ? name : self.map[name].proto;
 	// create the object
-	var obj = Object.create(req);
+	var obj = Object.create(proto);
 	
 	// add the node properties as top level properties on the object
 	// so that we don't have to do node._data.data every time
@@ -243,23 +207,9 @@ Forge.prototype.spawn = function(node, req, cb) {
 		obj.node = node;
 		obj.id = node.id;
 		obj.db = db;
+		obj.name = name;
 		
-		// Build navigation properties and some helper instance methods
-		// in parallel, since they have no depencies on one another
-		async.parallel([
-			function(callback){
-				self.buildNavigation(obj, req, function(err, obj){
-					callback(err);
-				});
-			},
-			function(callback){
-				self.buildInstance(obj, function(err, obj){
-					callback(err);
-				});
-			}
-		], function(err, results){
-			cb(err, obj);
-		});
+		self.buildInstance(name, obj, cb);
 	});
 }
 	
@@ -342,120 +292,22 @@ Forge.prototype.parseSet = function(options, set, callback) {
 /*
  * Create accessor methods for each type
  */
-Forge.prototype.buildMethods = function(req, file, cb) {
+Forge.prototype.buildMethods = function(file, cb) {
 	var self = this;
 	var obj = {
-		fromObj: function(obj, callback) { self.node.fromObj(req, arguments) },
-		fromId: function(id, callback) { self.node.fromId(req, arguments) },
-		fromIndex: function(index, key, value, callback) { self.node.fromIndex(req, arguments) },
-		fromNode: function(node, callback) { self.spawn(req, arguments) },
+		fromObj: function(obj, callback) { self.node.fromObj(arguments) },
+		fromId: function(id, callback) { self.node.fromId(arguments) },
+		fromIndex: function(index, key, value, callback) { self.node.fromIndex(arguments) },
+		fromNode: function(node, callback) { self.spawn(arguments) },
 		set: {
-			fromObjs: function(objs, options, callback) { self.node.set.fromObjs(req, arguments) },
-			fromIds: function(ids, options, callback) { self.node.set.fromIds(req, arguments) },
-			fromIndex: function(index, key, value, options, callback) { self.node.set.fromIndex(req, arguments) },
-			fromTemplate: function(obj, count, callback) { self.node.set.fromTemplate(req, arguments) },
-			fromQuery: function(cypher, vars, callback) { self.node.set.fromQuery(req, arguments) }
+			fromObjs: function(objs, options, callback) { self.node.set.fromObjs(arguments) },
+			fromIds: function(ids, options, callback) { self.node.set.fromIds(arguments) },
+			fromIndex: function(index, key, value, options, callback) { self.node.set.fromIndex(arguments) },
+			fromTemplate: function(obj, count, callback) { self.node.set.fromTemplate(arguments) },
+			fromQuery: function(cypher, vars, callback) { self.node.set.fromQuery(arguments) }
 		}
 	};
 	cb(obj);
-}
-
-/*
- * Sets up navigation properties for each model, based on paths defined in the module
- */
-Forge.prototype.buildNavigation = function(obj, req, cb) {
-	var self = this;
-	
-	// get navigation map
-	var navs = req.navigation || req.nav || req.navigation_properties || {};
-	var results = [];
-	
-	// loop over the keys
-	async.each(Object.keys(navs), function(nav, f){
-		
-		// and create a function on the object that returns node endpoints
-		obj[nav] = function(opts, callback){
-			if (typeof opts === 'function') {
-				callback = opts;
-				opts = {};
-			}
-			
-			var items = navs[nav].split(' ');
-			
-			// get the final node name so we can reference it later
-			var endpoint = items[items.length - 1];
-			var match = "MATCH st";
-			
-			// simple placeholder variables - shouldn't need more than 26
-			var names = "abcdefghijklmnopqrstuvwxyz".split("");
-			var used = [];
-			
-			// loop over each nav piece, needs to be done in series since the order matters
-			async.eachSeries(items, function(item, g){
-				
-				// if it's all caps, treat it as a relationship
-				if (item.match(/[A-Z_]+/)) {
-					
-					// get a temporary variable for this piece and put in used
-					var rel = names.shift();
-					used.push(rel);
-					
-					// construct a cypher relationsihp match in the form
-					// -[r:VERB]-> where r is the placeholder and VERB
-					// is the relationship type
-					match += '-[' + rel + ':' + item + ']->';
-					
-				} else {
-					var node = names.shift();
-					used.push(node);
-					match += node;
-				}
-				g();
-			}, function(err){
-				
-				// construct the cypher
-				var cypher = [
-					"START st=node({id})",
-					match
-				];
-				if (opts.where) cypher.push('WHERE ' + opts.where);
-				cypher.push('RETURN ' + end);
-				var query = cypher.join("\n");
-				
-				// get the last placeholder so we can return it
-				var end = used.pop();
-				
-				// query the graph
-				db.query(query, {id: obj.id}, function(err, matches) {
-					
-					// loop over the matches 
-					async.each(matches, function(match, g){
-						
-						// if this is a type that we have a proto for,
-						// make an object out of it and add it to the results
-						if (filemap[endpoint]) {
-							self.spawn(filemap[endpoint], match[end], function(err, obj){
-								results.push(obj);
-							});
-						} else {
-							
-							// otherwise, push the node itself
-							results.push(match[end]);
-						}
-					}, function(err){
-						
-						// finally, return the results
-						// this let's you make an object, then call
-						// object.nodesOfSomeType() and get nodes
-						// connected to this one
-						callback(err, results);
-					});
-				});
-			});
-		}
-	}, function(err){
-		cb(err, obj);
-	});
 }
 
 Forge.prototype.validate = function(obj, reqs, cb) {
@@ -489,24 +341,14 @@ Forge.prototype.buildInstance = function(obj, cb) {
 		async.each(Object.keys(obj), function(item, g){
 			
 			// stringify the object property for comparison
-			var i = JSON.stringify(obj[item]);
+			var i = typeof obj[item] === 'object' ? JSON.stringify(obj[item]) : obj[item];
 			var n = obj.node._data.data[item];
 			
-			// if it's changed and it's not a function
-			if (i && typeof i !== 'function' && i !== n) {
-				
-				// if it's an object, first stringify it since neo
-				// can't store more than simple types
-				if (typeof i === 'object') {
-					obj.node._data.data[item] = i;
-				} else {
-					obj.node._data.data[item] = i;
-				}
-			} else if (!obj[item]) {
-				// if the property has been removed, remove the corresponding
-				// node value
-				delete obj.node._data.data[item];
-			}
+			// If it's changed and it's not a function, update it.
+			// If it's gone, remove it
+			if (i && typeof i !== 'function' && i !== n) obj.node._data.data[item] = i;
+			else if (!i) delete obj.node._data.data[item];
+			
 		}, function(err){
 			// finally, perpetuate these changes
 			obj.node.save(f);
@@ -545,9 +387,8 @@ Forge.prototype.buildInstance = function(obj, cb) {
 		obj.node.index(arguments);
 	}
 	
-	// Maps to either node.createRelationshipTo or node.createRelationshipFrom depending
-	// on the value of dir.
-	obj.createRelationship = function(dir, otherNode, type, data, f) {
+	// Add a relationship between two nodes
+	obj.bind = function(dir, otherNode, type, data, f) {
 		if (!dir.match(/(to|from)/)) f('Acceptable directions are "to" and "from."');
 		var method = dir === 'to' ? 'createRelationshipTo' : 'createRelationshipFrom';
 		
@@ -564,6 +405,33 @@ Forge.prototype.buildInstance = function(obj, cb) {
 			} else obj.node[method](otherNode, type, data, f);
 		}
 	}
+	
+	// Remove a relationship between two nodes
+	obj.sever = function(otherNode, rel, f) {
+		var other = typeof otherNode === 'number' ? otherNode : otherNode.id;
+		if (typeof rel === 'function') f = rel, rel = '';
+		var cypher = [
+			"START n=node({id})",
+			"MATCH n-[r:" + rel.toUpperCase() + "]-x",
+			"WHERE x.id = {other}",
+			"DELETE r"
+		].join("\n");
+		self.db.query(cypher, {id: obj.id, other: other}, f);
+	}
+	
+	cb(null, obj);
+}
+
+Forge.prototype.getSchemaDir = function(cb) {
+	var dirs = "schema models protos neo objects".split(" ");
+	var dir;
+	async.each(dirs, function(item, f){
+		fs.exists(__dirname + '/' + item, function(err, exists){
+			if (exists) dir = item;
+		}, function(err){
+			cb(err, dir);
+		});
+	});
 }
 
 
@@ -572,4 +440,5 @@ Forge.prototype.buildInstance = function(obj, cb) {
  * TODO
  * 1. caching
  * 2. namespacing
+ * 3. navigation properties and other uses of schema.json
  */
---
+++ test/models/user.js
@@ -0,0 +1,7 @@
+module.exports = {
+	
+	games: function(f) {
+		
+	}
+	
+}
